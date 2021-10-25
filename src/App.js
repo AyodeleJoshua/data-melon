8e63e6322705c4af822838350195e29d4df22ae0
Joshua Ayodele <jayodele64@gmail.com>
2021-10-25 11:51:46 +0100

    More components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 
 import EnhancedTable from './components/EnhancedTable';
-import BarChart from './components/BarChart';
-import PieChart from './components/PieChart';
-import LineChart from './components/LineChart';
+import BarChartByYear from './components/BarChartByYear';
+import PieChartByYear from './components/PieChartByYear';
+import LineChartByYear from './components/LineChartByYear';
+import BarChartByCategory from './components/BarChartByCategory';
+import PieChartByCategory from './components/PieChartByCategory';
+import LineChartByCategory from './components/LineChartByCategory';
+import './App.css';
 
 const URL = 'https://g54qw205uk.execute-api.eu-west-1.amazonaws.com/DEV/stub';
 
@@ -26,79 +30,172 @@ function App() {
 		});
 	}, []);
 
-	const salesByYear = {
+	const StateSalesByYear = {
 		2014: { total: { sales: 0, profit: 0 } },
 		2015: { total: { sales: 0, profit: 0 } },
 		2016: { total: { sales: 0, profit: 0 } },
 		2017: { total: { sales: 0, profit: 0 } },
 		total: { sales: 0, profit: 0 },
 	};
-	
+	const categorySalesByYear = {
+		2014: { total: { sales: 0, profit: 0 } },
+		2015: { total: { sales: 0, profit: 0 } },
+		2016: { total: { sales: 0, profit: 0 } },
+		2017: { total: { sales: 0, profit: 0 } },
+		total: { sales: 0, profit: 0 },
+	};
+	const productSalesByYear = {
+		2014: { total: { sales: 0, profit: 0 } },
+		2015: { total: { sales: 0, profit: 0 } },
+		2016: { total: { sales: 0, profit: 0 } },
+		2017: { total: { sales: 0, profit: 0 } },
+		total: { sales: 0, profit: 0 },
+	};
+
 	if (data.length > 0) {
 		data.forEach((e) => {
 			const sales = Math.round(parseFloat(e['Sales']) * 100) / 100;
 			const profit = Math.round(parseFloat(e['Profit']) * 100) / 100;
 			if (e['Order Date'].substring(e['Order Date'].length - 4) === '2014') {
-				if (!salesByYear['2014'].hasOwnProperty(e['State'])) {
-					salesByYear['2014'][e['State']] = {};
-					salesByYear['2014'][e['State']].sales = sales;
-					salesByYear['2014'][e['State']].profit = profit;
+				if (!StateSalesByYear['2014'].hasOwnProperty(e['State'])) {
+					StateSalesByYear['2014'][e['State']] = {};
+					StateSalesByYear['2014'][e['State']].sales = sales;
+					StateSalesByYear['2014'][e['State']].profit = profit;
 				} else {
-					salesByYear['2014'][e['State']].sales += sales;
-					salesByYear['2014'][e['State']].profit += profit;
+					StateSalesByYear['2014'][e['State']].sales += sales;
+					StateSalesByYear['2014'][e['State']].profit += profit;
 				}
-				salesByYear['2014'].total.sales += sales;
-				salesByYear['2014'].total.profit += profit;
+				StateSalesByYear['2014'].total.sales += sales;
+				StateSalesByYear['2014'].total.profit += profit;
+				if (!categorySalesByYear['2014'].hasOwnProperty(e['Category'])) {
+					categorySalesByYear['2014'][e['Category']] = {};
+					categorySalesByYear['2014'][e['Category']].sales = sales;
+					categorySalesByYear['2014'][e['Category']].profit = profit;
+				} else {
+					categorySalesByYear['2014'][e['Category']].sales = sales;
+					categorySalesByYear['2014'][e['Category']].profit = profit;
+				}
+				if (!productSalesByYear['2014'].hasOwnProperty(e['Product Name'])) {
+					productSalesByYear['2014'][e['Product Name']] = {};
+					productSalesByYear['2014'][e['Product Name']].sales = sales;
+					productSalesByYear['2014'][e['Product Name']].profit = profit;
+				} else {
+					productSalesByYear['2014'][e['Product Name']].sales = sales;
+					productSalesByYear['2014'][e['Product Name']].profit = profit;
+				}
 			} else if (
 				e['Order Date'].substring(e['Order Date'].length - 4) === '2015'
 			) {
-				if (!salesByYear['2015'].hasOwnProperty(e['State'])) {
-					salesByYear['2015'][e['State']] = {};
-					salesByYear['2015'][e['State']].sales = sales;
-					salesByYear['2015'][e['State']].profit = profit;
+				if (!StateSalesByYear['2015'].hasOwnProperty(e['State'])) {
+					StateSalesByYear['2015'][e['State']] = {};
+					StateSalesByYear['2015'][e['State']].sales = sales;
+					StateSalesByYear['2015'][e['State']].profit = profit;
 				} else {
-					salesByYear['2015'][e['State']].sales += sales;
-					salesByYear['2015'][e['State']].profit += profit;
+					StateSalesByYear['2015'][e['State']].sales += sales;
+					StateSalesByYear['2015'][e['State']].profit += profit;
 				}
-				salesByYear['2015'].total.sales += sales;
-				salesByYear['2015'].total.profit += profit;
+				StateSalesByYear['2015'].total.sales += sales;
+				StateSalesByYear['2015'].total.profit += profit;
+				if (!categorySalesByYear['2015'].hasOwnProperty(e['Category'])) {
+					categorySalesByYear['2015'][e['Category']] = {};
+					categorySalesByYear['2015'][e['Category']].sales = sales;
+					categorySalesByYear['2015'][e['Category']].profit = profit;
+				} else {
+					categorySalesByYear['2015'][e['Category']].sales = sales;
+					categorySalesByYear['2015'][e['Category']].profit = profit;
+				}
+				if (!productSalesByYear['2015'].hasOwnProperty(e['Product Name'])) {
+					productSalesByYear['2015'][e['Product Name']] = {};
+					productSalesByYear['2015'][e['Product Name']].sales = sales;
+					productSalesByYear['2015'][e['Product Name']].profit = profit;
+				} else {
+					productSalesByYear['2015'][e['Product Name']].sales = sales;
+					productSalesByYear['2015'][e['Product Name']].profit = profit;
+				}
 			} else if (
 				e['Order Date'].substring(e['Order Date'].length - 4) === '2016'
 			) {
-				if (!salesByYear['2016'].hasOwnProperty(e['State'])) {
-					salesByYear['2016'][e['State']] = {};
-					salesByYear['2016'][e['State']].sales = sales;
-					salesByYear['2016'][e['State']].profit = profit;
+				if (!StateSalesByYear['2016'].hasOwnProperty(e['State'])) {
+					StateSalesByYear['2016'][e['State']] = {};
+					StateSalesByYear['2016'][e['State']].sales = sales;
+					StateSalesByYear['2016'][e['State']].profit = profit;
 				} else {
-					salesByYear['2016'][e['State']].sales += sales;
-					salesByYear['2016'][e['State']].profit += profit;
+					StateSalesByYear['2016'][e['State']].sales += sales;
+					StateSalesByYear['2016'][e['State']].profit += profit;
 				}
-				salesByYear['2016'].total.sales += sales;
-				salesByYear['2016'].total.profit += profit;
+				StateSalesByYear['2016'].total.sales += sales;
+				StateSalesByYear['2016'].total.profit += profit;
+				if (!categorySalesByYear['2016'].hasOwnProperty(e['Category'])) {
+					categorySalesByYear['2016'][e['Category']] = {};
+					categorySalesByYear['2016'][e['Category']].sales = sales;
+					categorySalesByYear['2016'][e['Category']].profit = profit;
+				} else {
+					categorySalesByYear['2016'][e['Category']].sales = sales;
+					categorySalesByYear['2016'][e['Category']].profit = profit;
+				}
+				if (!productSalesByYear['2016'].hasOwnProperty(e['Product Name'])) {
+					productSalesByYear['2016'][e['Product Name']] = {};
+					productSalesByYear['2016'][e['Product Name']].sales = sales;
+					productSalesByYear['2016'][e['Product Name']].profit = profit;
+				} else {
+					productSalesByYear['2016'][e['Product Name']].sales = sales;
+					productSalesByYear['2016'][e['Product Name']].profit = profit;
+				}
 			} else {
-				if (!salesByYear['2017'].hasOwnProperty(e['State'])) {
-					salesByYear['2017'][e['State']] = {};
-					salesByYear['2017'][e['State']].sales = sales;
-					salesByYear['2017'][e['State']].profit = profit;
+				if (!StateSalesByYear['2017'].hasOwnProperty(e['State'])) {
+					StateSalesByYear['2017'][e['State']] = {};
+					StateSalesByYear['2017'][e['State']].sales = sales;
+					StateSalesByYear['2017'][e['State']].profit = profit;
 				} else {
-					salesByYear['2017'][e['State']].sales += sales;
-					salesByYear['2017'][e['State']].profit += profit;
+					StateSalesByYear['2017'][e['State']].sales += sales;
+					StateSalesByYear['2017'][e['State']].profit += profit;
 				}
-				salesByYear['2017'].total.sales += sales;
-				salesByYear['2017'].total.profit += profit;
+				StateSalesByYear['2017'].total.sales += sales;
+				StateSalesByYear['2017'].total.profit += profit;
 			}
-			salesByYear.total.sales += sales;
-			salesByYear.total.profit += profit;
+			StateSalesByYear.total.sales += sales;
+			StateSalesByYear.total.profit += profit;
+			if (!categorySalesByYear['2017'].hasOwnProperty(e['Category'])) {
+				categorySalesByYear['2017'][e['Category']] = {};
+				categorySalesByYear['2017'][e['Category']].sales = sales;
+				categorySalesByYear['2017'][e['Category']].profit = profit;
+			} else {
+				categorySalesByYear['2017'][e['Category']].sales = sales;
+				categorySalesByYear['2017'][e['Category']].profit = profit;
+			}
+			if (!productSalesByYear['2017'].hasOwnProperty(e['Product Name'])) {
+				productSalesByYear['2017'][e['Product Name']] = {};
+				productSalesByYear['2017'][e['Product Name']].sales = sales;
+				productSalesByYear['2017'][e['Product Name']].profit = profit;
+			} else {
+				productSalesByYear['2017'][e['Product Name']].sales = sales;
+				productSalesByYear['2017'][e['Product Name']].profit = profit;
+			}
 		});
+
+		console.log(productSalesByYear);
 	}
 	return (
 		<React.Fragment>
 			<CssBaseline />
 			<Container maxWidth='lg'>
-				<BarChart data={salesByYear} />
-				<PieChart data={salesByYear} />
-				<LineChart data={salesByYear} />
-				<EnhancedTable tableData={data} />
+				<div className='charts'>
+					<div className='year'>
+						<BarChartByYear data={StateSalesByYear} />
+						<PieChartByYear data={StateSalesByYear} />
+						<LineChartByYear data={StateSalesByYear} />
+					</div>
+					{data.length > 0 ? (
+						<div className='category'>
+							<BarChartByCategory data={categorySalesByYear} />
+							<PieChartByCategory data={categorySalesByYear} />
+							<LineChartByCategory data={categorySalesByYear} />
+						</div>
+					) : null}
+				</div>
+				<div className='tables'>
+					<EnhancedTable tableData={data} />
+				</div>
 			</Container>
 		</React.Fragment>
 	);
---
+++ src/components/PieChartByCategory.js
@@ -0,0 +1,126 @@
+import { Pie } from 'react-chartjs-2';
+
+const BarChartByCategory = ({ data }) => {
+	return (
+		<div>
+			{/* bar */}
+			<Pie
+				data={{
+					labels: ['Furniture', 'Office Supplies', 'Technology'],
+					datasets: [
+						{
+							label: '2014 Sales',
+							data: [
+								data['2014']['Furniture'].sales,
+								data['2014']['Office Supplies'].sales,
+								data['2014']['Technology'].sales,
+							],
+							backgroundColor: [
+								'rgba(255, 99, 132, 0.2)',
+								'rgba(255, 99, 132, 0.2)',
+								'rgba(255, 99, 132, 0.2)',
+								// 'rgba(54, 162, 235, 0.2)',
+								// 'rgba(255, 206, 86, 0.2)',
+								// 'rgba(75, 192, 192, 0.2)',
+							],
+							borderColor: [
+								// 'rgba(255, 99, 132, 1)',
+								'rgba(54, 162, 235,1)',
+								'rgba(54, 162, 235,1)',
+								'rgba(54, 162, 235,1)',
+								// 'rgba(255, 206, 86, 1)',
+								// 'rgba(75, 192, 192, 1)',
+							],
+							borderWidth: 1,
+						},
+						{
+							label: '2015 Sales',
+							data: [
+								data['2015']['Furniture'].sales,
+								data['2015']['Office Supplies'].sales,
+								data['2015']['Technology'].sales,
+							],
+							backgroundColor: [
+								// 'rgba(255, 99, 132, 0.2)',
+								// 'rgba(54, 162, 235, 0.2)',
+								'rgba(255, 206, 86, 0.2)',
+								'rgba(255, 206, 86, 0.2)',
+								'rgba(255, 206, 86, 0.2)',
+								// 'rgba(75, 192, 192, 0.2)',
+							],
+							borderColor: [
+								// 'rgba(255, 99, 132, 1)',
+								// 'rgba(54, 162, 235,1)',
+								// 'rgba(255, 206, 86, 1)',
+								'rgba(75, 192, 192, 1)',
+								'rgba(75, 192, 192, 1)',
+								'rgba(75, 192, 192, 1)',
+							],
+							borderWidth: 1,
+						},
+						{
+							label: '2016 Sales',
+							data: [
+								data['2016']['Furniture'].sales,
+								data['2016']['Office Supplies'].sales,
+								data['2016']['Technology'].sales,
+							],
+							backgroundColor: [
+								// 'rgba(255, 99, 132, 0.2)',
+								// 'rgba(54, 162, 235, 0.2)',
+								// 'rgba(255, 206, 86, 0.2)',
+								// 'rgba(75, 19, 195, 0.2)',
+								'rgba(255,0,0,0.2)',
+								'rgba(255,0,0,0.2)',
+								'rgba(255,0,0,0.2)',
+							],
+							borderColor: [
+								'rgba(255, 99, 132, 1)',
+								'rgba(54, 162, 235,1)',
+								'rgba(255, 206, 86, 1)',
+								'rgba(75, 192, 192, 1)',
+							],
+							borderWidth: 1,
+						},
+						{
+							label: '2017 Sales',
+							data: [
+								data['2017']['Furniture'].sales,
+								data['2017']['Office Supplies'].sales,
+								data['2017']['Technology'].sales,
+							],
+							backgroundColor: [
+								'rgba(255, 99, 132, 0.2)',
+								'rgba(54, 162, 235, 0.2)',
+								'rgba(255, 206, 86, 0.2)',
+								'rgba(75, 192, 192, 0.2)',
+							],
+							borderColor: [
+								'rgba(255, 99, 132, 1)',
+								'rgba(54, 162, 235,1)',
+								'rgba(255, 206, 86, 1)',
+								'rgba(75, 192, 192, 1)',
+							],
+							borderWidth: 1,
+						},
+					],
+				}}
+				height={'400px'}
+				width={'600px'}
+				options={{
+					maintainAspectRatio: false,
+					// scales: {
+					// 	x: {
+					// 		stacked: true,
+					// 	},
+					// 	y: {
+					// 		stacked: true,
+					// 	},
+					// },
+				}}
+			/>
+		</div>
+	);
+};
+
+export default BarChartByCategory;
